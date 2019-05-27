7358e5cbf6b32c9d695d612737ed9fe1b8354cba
chi <w121210@hotmail.com>
2019-05-27 23:59:59 +0800

    .

--- baseline/train_renderer.py
+++ baseline/train_renderer.py
@@ -1,6 +1,7 @@
 import cv2
 import torch
 import numpy as np
+import argparse
 
 import torch.nn as nn
 import torch.nn.functional as F
@@ -19,24 +20,20 @@ draw_fn = draw_rect
 
 criterion = nn.MSELoss()
 net = FCN(num_input=action_dim)
-optimizer = optim.Adam(net.parameters(), lr=3e-6)
-batch_size = 4096
-
 use_cuda = torch.cuda.is_available()
-step = 0
 
 
-def save_model():
+def save_model(path="./renderer.pt"):
     if use_cuda:
         net.cpu()
-    torch.save(net.state_dict(), "./renderer.pkl")
+    torch.save(net.state_dict(), path)
     if use_cuda:
         net.cuda()
     print("saved model")
 
 
-def load_weights():
-    pretrained_dict = torch.load("./renderer.pkl")
+def load_weights(path="./renderer.pt"):
+    pretrained_dict = torch.load()
     model_dict = net.state_dict()
     pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
     model_dict.update(pretrained_dict)
@@ -44,59 +41,73 @@ def load_weights():
     print("loaded pretrained weights")
 
 
-# load_weights()
+def train(resume, output):
+    optimizer = optim.Adam(net.parameters(), lr=3e-6)
+    batch_size = 64
 
-# while step < 500000:
-while step < 10000:
-    net.train()
-    x = []
-    gt = []
+    step = 0
 
-    # generate ground truth data
-    for i in range(batch_size):
-        # _x = np.random.uniform(0, 1, action_dim)
-        _x = np.array([0, 0, 0.5, 0.5])
-        x.append(_x)
-        gt.append(draw_fn(_x))
+    if resume is not None:
+        load_weights(resume)
 
-    x = torch.tensor(x).float()
-    gt = torch.tensor(gt).float()
-    if use_cuda:
-        net = net.cuda()
-        x = x.cuda()
-        gt = gt.cuda()
+    while step < 500000:
+        net.train()
+        x = []
+        gt = []
 
-    y = net(x)
-    # print(gt)
-    # print(y)
-    optimizer.zero_grad()
-    loss = criterion(y, gt)
-    loss.backward()
-    optimizer.step()
-    print(step, loss.item())
+        # generate ground truth data
+        for i in range(batch_size):
+            # _x = np.random.uniform(0, 1, action_dim)
+            _x = np.array([0, 0, 0.5, 0.5])
+            x.append(_x)
+            gt.append(draw_fn(_x))
 
-    if step < 200000:
-        lr = 1e-4
-    elif step < 400000:
-        lr = 1e-5
-    else:
-        lr = 1e-6
+        x = torch.tensor(x).float()
+        gt = torch.tensor(gt).float()
+        if use_cuda:
+            net = net.cuda()
+            x = x.cuda()
+            gt = gt.cuda()
 
-    for param_group in optimizer.param_groups:
-        param_group["lr"] = lr
-    writer.add_scalar("train/loss", loss.item(), step)
-
-    if step % 100 == 0:
-        net.eval()
         y = net(x)
+        # print(gt)
+        # print(y)
+        optimizer.zero_grad()
         loss = criterion(y, gt)
-        writer.add_scalar("val/loss", loss.item(), step)
-        for i in range(32):
-            G = y[i].cpu().data.numpy()
-            GT = gt[i].cpu().data.numpy()
-            writer.add_image("train/gen{}.png".format(i), G, step)
-            writer.add_image("train/ground_truth{}.png".format(i), GT, step)
+        loss.backward()
+        optimizer.step()
+        print(step, loss.item())
 
-    if step % 1000 == 0:
-        save_model()
-    step += 1
+        if step < 200000:
+            lr = 1e-4
+        elif step < 400000:
+            lr = 1e-5
+        else:
+            lr = 1e-6
+
+        for param_group in optimizer.param_groups:
+            param_group["lr"] = lr
+        writer.add_scalar("train/loss", loss.item(), step)
+
+        if step % 100 == 0:
+            net.eval()
+            y = net(x)
+            loss = criterion(y, gt)
+            writer.add_scalar("val/loss", loss.item(), step)
+            for i in range(32):
+                G = y[i].cpu().data.numpy()
+                GT = gt[i].cpu().data.numpy()
+                writer.add_image("train/gen{}.png".format(i), G, step)
+                writer.add_image("train/ground_truth{}.png".format(i), GT, step)
+
+        if step % 1000 == 0:
+            save_model(output)
+        step += 1
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output", default="./renderer.pt", type=str)
+    parser.add_argument("--resume", default=None, type=str)
+    args = parser.parse_args()
+    train(args.resume, args.output)
